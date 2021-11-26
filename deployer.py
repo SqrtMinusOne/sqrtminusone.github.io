a287df963aecc1862fb3a8d05424fee51bc1889f
SqrtMinusOne <thexcloud@gmail.com>
2021-11-26 18:54:46 +0300

    feat: some kind of deployer

---
+++ deployer.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+from flask import Flask
+
+from dotenv import load_dotenv
+from flask_httpauth import HTTPTokenAuth
+
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    auth = HTTPTokenAuth(scheme='Bearer')
+
+    @auth.verify_token
+    def verify_token(token):
+        return token == os.environ['TOKEN']
+
+    @app.route('/', methods=['POST'])
+    @auth.login_required
+    def deploy():
+        subprocess.run(['git', 'fetch', 'origin'])
+        subprocess.run(['git', 'checkout', 'origin/gh-pages'])
+
+    return app
