48b23984999660679596acaf213f685cd90db508
SqrtMinusOne <thexcloud@gmail.com>
2021-12-11 21:48:24 +0300

    feat: screw Docker

--- deploy/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-CWD=/usr/share/nginx/html
-(cd $CWD && git fetch origin && git checkout origin/gh-pages)
-(cd /deploy && gunicorn -w 1 "deployer:create_app()") &
-nginx -g 'daemon off;'
