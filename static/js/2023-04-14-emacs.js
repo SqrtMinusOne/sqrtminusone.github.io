0633a9bdefc1c9999f41557514b5df9ab1a7e8e6
SqrtMinusOne <thexcloud@gmail.com>
2023-04-04 23:28:29 +0300

    feat(emacs): 1rst chart works as expected

--- static/js/2023-04-14-emacs.js
+++ static/js/2023-04-14-emacs.js
@@ -1,22 +1,130 @@
-function initChart() {
-  const ctx = document.getElementById("myChart");
+const TODAY = new Date("2023-04-14");
+const TODAY_LOCALE = TODAY.toLocaleDateString("en-GB");
 
-  new Chart(ctx, {
-    type: "bar",
-    data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-      datasets: [
+const EMACS_DATA = {
+  labels: ["Editor/IDE", "File manager"],
+  datasets: [
+    {
+      label: "Jupyter",
+      data: [
         {
-          label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1,
+          name: "Editor/IDE",
+          span: [new Date("2018-10-24"), new Date("2021-04-01")],
         },
       ],
+      yAxisID: "yAxis1",
     },
+    {
+      label: "NeoVim",
+      data: [
+        {
+          name: "Editor/IDE",
+          span: [new Date("2019-03-30"), new Date("2020-10-12")],
+        },
+      ],
+      yAxisID: "yAxis1",
+    },
+    {
+      label: "DataGrip",
+      data: [
+        {
+          name: "Editor/IDE",
+          span: [new Date("2020-02-01"), TODAY],
+        },
+      ],
+      yAxisID: "yAxis1",
+    },
+    {
+      label: "Emacs",
+      data: [
+        {
+          name: "Editor/IDE",
+          span: [new Date("2020-10-12"), TODAY],
+        },
+      ],
+      yAxisID: "yAxis1",
+    },
+    {
+      label: "vifm",
+      data: [
+        {
+          name: "File manager",
+          span: [new Date("2020-02-17"), new Date("2020-11-11")],
+        },
+      ],
+      yAxisID: "yAxis2",
+    },
+    {
+      label: "Dired",
+      data: [
+        {
+          name: "File manager",
+          span: [new Date("2020-11-11"), TODAY],
+        },
+      ],
+      yAxisID: "yAxis2",
+    },
+  ],
+};
+
+function emacsChart() {
+  const ctx = document.getElementById("chart-emacs-history");
+  new Chart(ctx, {
+    type: "bar",
+    data: EMACS_DATA,
+    plugins: [ChartDataLabels],
     options: {
+      indexAxis: "y",
+      grouped: true,
+      parsing: {
+        yAxisKey: "name",
+        xAxisKey: "span",
+      },
+      layout: {
+        padding: 1,
+      },
       scales: {
-        y: {
-          beginAtZero: true,
+        x: {
+          type: "time",
+          min: new Date("2018-06"),
+        },
+        yAxis1: {
+          ticks: {
+            display: false,
+          },
+        },
+        yAxis2: {
+          ticks: {
+            // display: false
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          display: false,
+        },
+        datalabels: {
+          formatter: function (value, context) {
+            return context.dataset.label;
+          },
+          color: "black",
+        },
+        tooltip: {
+          callbacks: {
+            label: function (context) {
+              const startDate = new Date(
+                context.parsed._custom.start
+              ).toLocaleDateString("en-GB");
+              let endDate = new Date(
+                context.parsed._custom.end
+              ).toLocaleDateString("en-GB");
+              if (endDate === TODAY_LOCALE) {
+                endDate = "Today";
+              }
+              const label = context.dataset.label;
+              return `${label}: ${startDate} - ${endDate}`;
+            },
+          },
         },
       },
     },
@@ -26,7 +134,7 @@ function initChart() {
 document.addEventListener(
   "DOMContentLoaded",
   function () {
-    initChart();
+    emacsChart();
   },
   false
 );
